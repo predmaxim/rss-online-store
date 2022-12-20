6757551e0e115e865818b52772677d3bc88a8308
Maxim Predybailo <predmaxim@ya.ru>
2022-12-20 18:31:21 +0500

    chore: cleare garbage

--- dist/test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const a = 'csdscdsd';
-const b = 'fv';
-const ddd = (a, b) => {
-  console.log(a + b);
-};
-ddd(a, b);
--- src/test.ts
+++
@@ -1,8 +0,0 @@
-const a = 'csdscdsd';
-const b = 'gsddg';
-
-const ddd = (a: string, b: string) => {
-  console.log(a + b);
-};
-
-ddd(a, b);
