7b5e59e33dc92191c1718fd4ef2eac70ab10b4a6
Maxim Predybailo <predmaxim@ya.ru>
2022-12-23 02:10:07 +0500

    refactor: shop header

---
+++ src/components/shop-header/shop-header.ts
@@ -0,0 +1,7 @@
+// const navIcon: HTMLDivElement = <HTMLDivElement>document.querySelector('.nav-icon');
+// const menu: HTMLDivElement = <HTMLDivElement>document.querySelector('.menu');
+
+// navIcon.addEventListener('click', (): void => {
+//   menu.classList.toggle('hide');
+//   navIcon.classList.toggle('active');
+// });
