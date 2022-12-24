449af02c9282ff48a9a78b56cc86d1d22bb6304f
Maxim Predybailo <predmaxim@ya.ru>
2022-12-24 18:33:55 +0500

    feat: add filter

---
+++ src/components/filter/filter.ts
@@ -0,0 +1,14 @@
+const filterIcon: HTMLDivElement = <HTMLDivElement>document.querySelector('.filter-icon');
+const filter: HTMLDivElement = <HTMLDivElement>document.querySelector('.filter-container');
+
+filterIcon.addEventListener('click', (): void => {
+  filter.classList.toggle('hide');
+  filterIcon.classList.toggle('active');
+});
+
+const mql = window.matchMedia('(max-width: 640px)');
+
+if (mql.matches) {
+  filterIcon.classList.toggle('hide');
+  filter.classList.toggle('hide');
+}
