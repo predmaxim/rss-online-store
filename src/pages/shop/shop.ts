c4fe7bb7d17be2a097747cb2a68f640383ece62e
Maxim Predybailo <predmaxim@ya.ru>
2022-12-24 18:36:22 +0500

    refactor: layout

---
+++ src/pages/shop/shop.ts
@@ -0,0 +1,7 @@
+import 'ShopHeader/shop-header';
+import 'MiniCart/mini-cart';
+import 'Filter/filter';
+import 'View/view';
+import 'Sort/sort';
+import 'ProductGrid/product-grid';
+import 'Sort/sort';
