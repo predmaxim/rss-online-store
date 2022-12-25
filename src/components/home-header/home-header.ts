73a084c1281b28b7b4d11a805ad1d123572cec76
Maxim Predybailo <predmaxim@ya.ru>
2022-12-25 21:10:29 +0500

    feat: add func activate menu items

--- src/components/home-header/home-header.ts
+++ src/components/home-header/home-header.ts
@@ -1,8 +1,18 @@
 const navIcon: HTMLDivElement = <HTMLDivElement>document.querySelector('.nav-icon');
 const menu: HTMLDivElement = <HTMLDivElement>document.querySelector('.menu');
-const menuItems = document.querySelector('.menu__item');
+const menuItems: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('.menu__item');
+
+function activeItem(itemList: NodeListOf<HTMLAnchorElement>): void {
+  [...itemList].forEach((el) => {
+    el.href === window.location.href ? el.classList.toggle('active') : false;
+  });
+}
 
 navIcon.addEventListener('click', (): void => {
   menu.classList.toggle('hide');
   navIcon.classList.toggle('active');
 });
+
+activeItem(menuItems);
+
+export { activeItem };
