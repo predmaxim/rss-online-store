8de012a211a749c6ce1641557c6080305901e69d
Iryna Ishchanka <28myhome@gmail.com>
2022-12-27 11:48:20 +0300

    feat: add data-folder

---
+++ src/data/info.ts
@@ -0,0 +1,393 @@
+import { Card } from "Card"
+
+export const arrCards: Card[] = [
+  {
+    "id": 1,
+    "name": "Wool yarn",
+    "category": "yarn",
+    "description": "100% wool, assorted colors. Check availability of colors and yarn types.",
+    "year": 2021,
+    "price": 17,
+    "stock": 500,
+    "images": [
+      "./img/1-1.jpg",
+      "./img/1-1.jpg",
+      "./img/1-1.jpg"
+    ]
+  },
+
+  {
+    "id": 2,
+    "name": "Scarf S-2110",
+    "category": "scarf",
+    "description": "Light gray color, size: 180x30cm, 100% merino wool",
+    "year": 2021,
+    "price": 208,
+    "stock": 5,
+    "images": [
+      "./img/2-1.jpg",
+      "./img/2-2.jpg",
+      "./img/2-3.jpg"
+    ]
+  },
+
+  {
+    "id": 3,
+    "name": "Scarf L-1572",
+    "category": "scarf",
+    "description": "Blue gray color, size: 190x70cm, 100% merino wool",
+    "year": 2022,
+    "price": 348,
+    "stock": 12,
+    "images": [
+      "./img/3-1.jpg",
+      "./img/3-2.jpg",
+      "./img/3-3.jpg"
+    ]
+  },
+
+  {
+    "id": 4,
+    "name": "Scarf C-6031",
+    "category": "scarf",
+    "description": "Classic gray color, size: 210x30cm, 100% merino wool",
+    "year": 2020,
+    "price": 285,
+    "stock": 14,
+    "images": [
+      "./img/4-1.jpg",
+      "./img/4-2.jpg",
+      "./img/4-3.jpg"
+    ]
+  },
+
+  {
+    "id": 5,
+    "name": "Scarf S-2120",
+    "category": "scarf",
+    "description": "Dark gray color, size: 180x30cm, 100% wool",
+    "year": 2021,
+    "price": 208,
+    "stock": 4,
+    "images": [
+      "./img/5-1.jpg",
+      "./img/5-2.jpg",
+      "./img/5-3.jpg"
+    ]
+  },
+
+  {
+    "id": 6,
+    "name": "Scarf C-6005",
+    "category": "scarf",
+    "description": "Classic gray color with stripes, size: 210x30cm, 100% wool",
+    "year": 2020,
+    "price": 285,
+    "stock": 2,
+    "images": [
+      "./img/6-1.jpg",
+      "./img/6-2.jpg",
+      "./img/6-3.jpg"
+    ]
+  },
+
+  {
+    "id": 7,
+    "name": "Scarf B-7715",
+    "category": "scarf",
+    "description": "Bright with checkered colors, size: 210x30cm, 100% wool",
+    "year": 2019,
+    "price": 300,
+    "stock": 1,
+    "images": [
+      "./img/7-1.jpg",
+      "./img/7-2.jpg",
+      "./img/7-3.jpg"
+    ]
+  },
+
+  {
+    "id": 8,
+    "name": "Scarf B-1180",
+    "category": "scarf",
+    "description": "Bright color with stripes, size: 180x30cm, 100% wool",
+    "year": 2021,
+    "price": 285,
+    "stock": 2,
+    "images": [
+      "./img/8-1.jpg",
+      "./img/8-2.jpg",
+      "./img/8-3.jpg"
+    ]
+  },
+
+  {
+    "id": 9,
+    "name": "Scarf B-2280",
+    "category": "scarf",
+    "description": "Dark purple color, size: 1900x70cm, 100% merino wool",
+    "year": 2022,
+    "price": 230,
+    "stock": 28,
+    "images": [
+      "./img/9-1.jpg",
+      "./img/9-2.jpg",
+      "./img/9-3.jpg"
+    ]
+  },
+
+  {
+    "id": 10,
+    "name": "Scarf S-2270",
+    "category": "scarf",
+    "description": "Dark red color, size: 210x30cm, 100% merino wool",
+    "year": 2022,
+    "price": 210,
+    "stock": 20,
+    "images": [
+      "./img/10-1.jpg",
+      "./img/10-2.jpg",
+      "./img/10-3.jpg"
+    ]
+  },
+
+  {
+    "id": 11,
+    "name": "Plaid P-1220",
+    "category": "plaid",
+    "description": "Gray with a Christmas tree pattern, size: 200x130cm, 100% merino wool",
+    "year": 2022,
+    "price": 450,
+    "stock": 57,
+    "images": [
+      "./img/11-1.jpg",
+      "./img/1-2.jpg",
+      "./img/11-3.jpg"
+    ]
+  },
+
+  {
+    "id": 12,
+    "name": "Plaid P-1030",
+    "category": "plaid",
+    "description": "Gray sheep pattern, size: 200x130cm, 100% merino wool",
+    "year": 2022,
+    "price": 450,
+    "stock": 68,
+    "images": [
+      "./img/12-1.jpg",
+      "./img/12-2.jpg",
+      "./img/12-3.jpg"
+    ]
+  },
+
+  {
+    "id": 13,
+    "name": "Plaid P-1070",
+    "category": "plaid",
+    "description": "Brown sheep pattern, size: 200x130cm, 100% merino wool",
+    "year": 2022,
+    "price": 450,
+    "stock": 20,
+    "images": [
+      "./img/13-1.jpg",
+      "./img/13-2.jpg",
+      "./img/13-3.jpg"
+    ]
+  },
+
+  {
+    "id": 14,
+    "name": "Plaid P-2096",
+    "category": "plaid",
+    "description": "Red checkered, size: 210x130cm, 100% merino wool",
+    "year": 2022,
+    "price": 470,
+    "stock": 65,
+    "images": [
+      "./img/14-1.jpg",
+      "./img/14-2.jpg",
+      "./img/14-3.jpg"
+    ]
+  },
+
+  {
+    "id": 15,
+    "name": "Plaid P-3017",
+    "category": "plaid",
+    "description": "Light gray with deer, size: 200x130cm, 100% merino wool",
+    "year": 2020,
+    "price": 360,
+    "stock": 5,
+    "images": [
+      "./img/15-1.jpg",
+      "./img/15-2.jpg",
+      "./img/15-3.jpg"
+    ]
+  },
+
+  {
+    "id": 16,
+    "name": "Plaid S-7171",
+    "category": "plaid",
+    "description": "Scottish cell, size: 210x130cm, 100% merino wool",
+    "year": 2017,
+    "price": 370,
+    "stock": 10,
+    "images": [
+      "./img/16-1.jpg",
+      "./img/16-2.jpg",
+      "./img/16-3.jpg"
+    ]
+  },
+
+  {
+    "id": 17,
+    "name": "Plaid S-7000",
+    "category": "plaid",
+    "description": "Gray with bears, size: 210x130cm, 100% wool",
+    "year": 2021,
+    "price": 310,
+    "stock": 35,
+    "images": [
+      "./img/17-1.jpg",
+      "./img/17-2.jpg",
+      "./img/17-3.jpg"
+    ]
+  },
+
+  {
+    "id": 18,
+    "name": "Plaid S-8501",
+    "category": "plaid",
+    "description": "Bright cell, size: 210x130cm, 100% merino wool",
+    "year": 2022,
+    "price": 420,
+    "stock": 76,
+    "images": [
+      "./img/18-1.jpg",
+      "./img/18-2.jpg",
+      "./img/18-3.jpg"
+    ]
+  },
+
+  {
+    "id": 19,
+    "name": "Plaid S-8502",
+    "category": "plaid",
+    "description": "Blue cell, size: 210x130cm, 100% merino wool",
+    "year": 2022,
+    "price": 420,
+    "stock": 4,
+    "images": [
+      "./img/19-1.jpg",
+      "./img/19-2.jpg",
+      "./img/19-3.jpg"
+    ]
+  },
+
+  {
+    "id": 20,
+    "name": "Plaid S-6304",
+    "category": "plaid",
+    "description": "Gray with cats, size: 210x130cm, 100% merino wool",
+    "year": 2018,
+    "price": 400,
+    "stock": 5,
+    "images": [
+      "./img/20-1.jpg",
+      "./img/20-2.jpg",
+      "./img/20-3.jpg"
+    ]
+  },
+
+  {
+    "id": 21,
+    "name": "Plaid S-6305",
+    "category": "plaid",
+    "description": "Gray with horses, size: 210x130cm, 100% merino wool",
+    "year": 2018,
+    "price": 400,
+    "stock": 2,
+    "images": [
+      "./img/21-1.jpg",
+      "./img/21-2.jpg",
+      "./img/21-3.jpg"
+    ]
+  },
+
+  {
+    "id": 22,
+    "name": "Hat H-1105",
+    "category": "hat",
+    "description": "Different colors, 100% merino wool",
+    "year": 2022,
+    "price": 200,
+    "stock": 200,
+    "images": [
+      "./img/22-1.jpg",
+      "./img/22-2.jpg",
+      "./img/22-3.jpg"
+    ]
+  },
+
+  {
+    "id": 23,
+    "name": "Hat H-2203",
+    "category": "hat",
+    "description": "Different colors, 100% merino wool",
+    "year": 2022,
+    "price": 185,
+    "stock": 200,
+    "images": [
+      "./img/23-1.jpg",
+      "./img/23-2.jpg",
+      "./img/23-3.jpg"
+    ]
+  },
+
+  {
+    "id": 24,
+    "name": "Hat H-1714",
+    "category": "hat",
+    "description": "Different colors, 100% merino wool",
+    "year": 2022,
+    "price": 250,
+    "stock": 183,
+    "images": [
+      "./img/24-1.jpg",
+      "./img/24-2.jpg",
+      "./img/24-3.jpg"
+    ]
+  },
+
+  {
+    "id": 25,
+    "name": "Hat H-1603",
+    "category": "hat",
+    "description": "Different colors, 100% merino wool",
+    "year": 2021,
+    "price": 194,
+    "stock": 7,
+    "images": [
+      "./img/25-1.jpg",
+      "./img/25-2.jpg",
+      "./img/25-3.jpg"
+    ]
+  },
+
+  {
+    "id": 26,
+    "name": "Hat H-1604",
+    "category": "hat",
+    "description": "Different colors, 100% merino wool",
+    "year": 2021,
+    "price": 194,
+    "stock": 7,
+    "images": [
+      "./img/26-1.jpg",
+      "./img/26-2.jpg",
+      "./img/26-3.jpg"
+    ]
+  }
+]
